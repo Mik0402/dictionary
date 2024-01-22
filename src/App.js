02ef864ae8fa2a01596fb96a2646e3df86b2bfe0
Mikaela Lammers <miklammers12@gmail.com>
2024-01-21 20:09:56 -0600

    Added initial structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>📚 Dictionary</h1>
+        <input placeholder='Search' />
+        <button type="Submit" className="btn btn-primary">Search</button>
       </header>
     </div>
   );
