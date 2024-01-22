ed87b3e3acf0577d9ee9fdeec3a5181b5b81e3b4
Mikaela Lammers <miklammers12@gmail.com>
2024-01-22 10:46:05 -0600

    Adding the meaning of the word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response);
+        setResults(response.data[0]);
     }
 
     function search(event) {
@@ -22,13 +24,13 @@ export default function Dictionary() {
 
     return (
         <div className="dictionary text-center">
-            
             <h1>📚 Dictionary</h1>
             <h2>What word do you want to look up?</h2>
             <form onSubmit={search}>
         <input type="search" autoFocus={true} placeholder=' Search' onChange={handleKeywordChange} />
         <button type="Submit" className="btn btn-primary">Search</button>
         </form>
+        <Results results={results} />
         </div>
     );
 }
