f8fbc79585b46381e77b45492f65cc6df534d30f
Mikaela Lammers <miklammers12@gmail.com>
2024-01-22 18:09:03 -0600

    Styling app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="sunset" />
       </main>
       <footer className="App-footer">
         <small>Coded by Mikaela Lammers</small>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,36 +3,55 @@ import axios from "axios";
 import Results from "./Results";
 import "./dictionary.css";
 
-export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+    let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
 
     function handleResponse(response) {
         setResults(response.data[0]);
     }
 
-    function search(event) {
-        event.preventDefault();
-
+    function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
     }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
 
+    function load() {
+        setLoaded(true);
+        search();
+    }
+
+    if (loaded) {
     return (
         <div className="dictionary">
             <div className="text-center">
             <h1>📚 Dictionary</h1>
+            </div>
+            <section>
             <h2>What word do you want to look up?</h2>
-            <form onSubmit={search}>
-        <input type="search" autoFocus={true} placeholder=' Search' onChange={handleKeywordChange} />
-        <button type="submit" className="btn btn-primary">Search</button>
-        </form>
-        </div>
-        <Results results={results} />
+            <form onSubmit={handleSubmit}>
+             <input type="search" autoFocus={true} placeholder=' Search' onChange={handleKeywordChange} defaultValue={props.defaultKeyword} />
+            </form>
+            <div className="hint">
+                suggested words: sunset, wine, dance, tree, etc.
+            </div>
+            </section>
+            
+            <Results results={results} />
         </div>
     );
+    } else {
+        load();
+        return "Loading...";
+    }
 }
