9dcace347e33ecc3ed056d8f3f6dfb2914cdd494
Mikaela Lammers <miklammers12@gmail.com>
2024-01-22 21:26:56 -0600

    importing pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,23 @@ export default function Dictionary(props) {
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data[0]);
+    }
+
+    function handlePexelResponse(response) {
+        console.log(response);
     }
 
     function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        let pexelsApiKey ="h8EH1BNqymVafp0MwsFzYx2VO9Vvxzrt5Iot4NF46MIPCOacRbDWbFFN";
+        let pexelsApiUrl =`https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+        let headers = { Authorization : `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers })
+    .then(handlePexelResponse);
     }
 
     function handleSubmit(event) {
@@ -46,8 +56,11 @@ export default function Dictionary(props) {
                 suggested words: sunset, wine, dance, tree, etc.
             </div>
             </section>
-            
+            <div className="row">
+                <div className="col-6">
             <Results results={results} />
+            </div>
+            </div>
         </div>
     );
     } else {
