0c8beb8a8b809627459f9d7ba5844cc4ecf7c893
Mikaela Lammers <miklammers12@gmail.com>
2024-01-23 20:54:51 -0600

    limiting the amount of photos per page

--- src/Photos.js
+++ src/Photos.js
@@ -6,13 +6,17 @@ export default function Photos(props) {
         return (
             <section className="Photos">
                 {props.photos.map(function(photo, index) {
+                    if (index < 3) {
                     return (
                         <div key={index}>
                             <a href={photo.src.original} target="_blank" rel="noreferrer">
-                        <img src={photo.src.landscape} className="img-fluid" alt="word"/>
-                        </a>
+                                <img src={photo.src.landscape} className="img-fluid" alt="word"/>
+                            </a>
                         </div>
                     );
+                    } else {
+                        return null;
+                    }
                 })}
             </section>
         );
